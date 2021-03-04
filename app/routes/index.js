2abb46206bce54a606d6afb627bf9da62183480a
sintya ridho <ridho@wahyoo.com>
2021-03-04 08:54:26 +0700

    fix: routes

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,11 +1,3 @@
 const app = module.exports = require('express')();
 
-app.get('/', async (req, res) => {
-    res.status(200).json({
-        code: 200,
-        status: 'success',
-        message: 'Welcome to the beginning of nothingness'
-    });
-});
-
 app.use('/', require('./movie')); // user routes
